6a33312880d09d9d8eee6a5ce86368b36c7c0806
Artmen_V <viliutich.artem@gmail.com>
2019-08-29 12:54:31 +0700

    draw_circle_angle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 function draw() {
-  var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    var context = canvas.getContext("2d");
-
-    context.beginPath();
-    context.moveTo(75, 75);
-    context.lineTo(10, 75);
-    context.lineTo(10, 25);
-    context.fill();
+  var ctx = document.getElementById("myCanvas").getContext("2d");
+  var counter = 0;
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < 6; j++) {
+      //Start from white and goes to black
+      ctx.fillStyle =
+        "rgb(" +
+        Math.floor(255 - 42.5 * i) +
+        "," +
+        Math.floor(255 - 42.5 * i) +
+        "," +
+        Math.floor(255 - 42.5 * j) +
+        ")";
+      ctx.beginPath();
+      if (i === counter && j === counter) {
+        //creates the circles
+        ctx.arc(25 + j * 50, 30 + i * 50, 20, 0, Math.PI * 2, true);
+        ctx.fill();
+        //creates a border around the circles so white one will be vissible
+        ctx.stroke();
+      }
+    }
+    counter++;
   }
 }
+draw();
